296bdf5555b44f76dcfeb04d7bf16abf1fd3b598
Vivek <vivekwagh63@gmail.com>
2020-09-06 04:16:42 +0530

    Add authentication for login and add create message method

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "User1", password: "user1")
+User.create(username: "User2", password: "user2")
+User.create(username: "User3", password: "user3")
+User.create(username: "User4", password: "user4")
+User.create(username: "User5", password: "user5")
+
+Message.create(body: "Hi How are you?", user: User.last)
+Message.create(body: "Hi I am good?", user: User.first)
